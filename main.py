f57fd174e397bae1b9b151e2cde0a51be35e02ce
sage <hello@sageelliott.com>
2022-10-23 16:14:20 -0700

    add initial touch sensor

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ led_left = Pin(16,Pin.OUT)
 photo_left = ADC(Pin(26))
 photo_right = ADC(Pin(27))
 
-
+touch_right = Pin(14, Pin.IN, Pin.PULL_DOWN)
 
 
 servo_left.freq(50)
@@ -50,24 +50,33 @@ light_left = photo_left.read_u16()
 light_right = photo_right.read_u16()
 
 
+
+
 utime.sleep(2)
 print(light_left)
 print(light_right)
 on_board_led.value(0)
 led_right.value(0)
 led_left.value(0)
-forward()
-utime.sleep(2)
-stop()
-utime.sleep(2)
-backward()
-utime.sleep(2)
-stop()
-utime.sleep(2)
-left()
-utime.sleep(2)
-stop()
-utime.sleep(2)
-right()
-utime.sleep(2)
-stop()
+
+while True:
+    if switch1.value() == 1:
+        led_right.value(1)
+    else:
+        led_right.value(0)
+
+#forward()
+#utime.sleep(2)
+#stop()
+#utime.sleep(2)
+#backward()
+#utime.sleep(2)
+#stop()
+#utime.sleep(2)
+#left()
+#utime.sleep(2)
+#stop()
+#utime.sleep(2)
+#right()
+#utime.sleep(2)
+#stop()
